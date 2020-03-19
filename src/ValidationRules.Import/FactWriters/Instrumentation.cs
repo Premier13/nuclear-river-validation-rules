1b822e9f294c7d45364e184d09ab79d332fe6c46
a.rechkalov <a.rechkalov@2gis.ru>
2020-03-19 17:25:42 +0700

    [ERM-13211] Добавил деталей в исключения

--- src/ValidationRules.Import/FactWriters/Instrumentation.cs
+++ src/ValidationRules.Import/FactWriters/Instrumentation.cs
@@ -7,19 +7,33 @@ namespace NuClear.ValidationRules.Import.FactWriters
     {
         public static T Do<T>(Func<T> func, string category, string name)
         {
-            var timer = Stopwatch.StartNew();
-            var result = func.Invoke();
-            timer.Stop();
-            Log.Debug(category, new {Name = name, Time = timer.Elapsed});
-            return result;
+            try
+            {
+                var timer = Stopwatch.StartNew();
+                var result = func.Invoke();
+                timer.Stop();
+                Log.Debug(category, new {Name = name, Time = timer.Elapsed});
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Writing failed on '{category}', '{name}'", e);
+            }
         }
 
         public static void Do(Action func, string category, string name)
         {
-            var timer = Stopwatch.StartNew();
-            func.Invoke();
-            timer.Stop();
-            Log.Debug(category, new {Name = name, Time = timer.Elapsed});
+            try
+            {
+                var timer = Stopwatch.StartNew();
+                func.Invoke();
+                timer.Stop();
+                Log.Debug(category, new {Name = name, Time = timer.Elapsed});
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Writing failed on '{category}', '{name}'", e);
+            }
         }
     }
 }
